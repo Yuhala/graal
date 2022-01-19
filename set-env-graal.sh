44c59112712b99afdc7a929f194116fe8a5a7a65
Yuhala <petersonyuhala@gmail.com>
2022-01-19 16:12:10 +0100

    updated env script

--- set-env-graal.sh
+++ set-env-graal.sh
@@ -4,5 +4,5 @@
 # adds mx to path and points java home to jdk-jvmci
 #
 
-export PATH=$PWD/vm/latest_graalvm/graalvm-26855d62ff-java8-21.1.0-dev/bin:$PATH
-export JAVA_HOME=$PWD/vm/latest_graalvm/graalvm-26855d62ff-java8-21.1.0-dev
+export PATH=$PWD/vm/latest_graalvm/graalvm-244b5a8878-java11-22.1.0-dev/bin:$PATH
+export JAVA_HOME=$PWD/vm/latest_graalvm/graalvm-244b5a8878-java11-22.1.0-dev
