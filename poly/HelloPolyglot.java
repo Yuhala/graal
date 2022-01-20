25e7178e28e381e671e117d0250e406a664df218
Yuhala <petersonyuhala@gmail.com>
2022-01-20 15:47:56 +0100

    testing inner context

--- poly/HelloPolyglot.java
+++ poly/HelloPolyglot.java
@@ -1,3 +1,4 @@
+
 import org.graalvm.polyglot.*;
 import org.graalvm.polyglot.proxy.*;
 
@@ -5,8 +6,21 @@ public class HelloPolyglot {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
         try (Context context = Context.create()) {
-		System.out.println("About to run js context");
-    		context.eval("js", "print('Hello JavaScript!');");
+            System.out.println("About to run js context");
+            context.eval("js", "print('Hello JavaScript!');");
+        }
+
+        innerContext();
+    }
+
+    static void innerContext() {
+        try (Context outer = Context.newBuilder()
+                .allowAllAccess(true)
+                .build()) {
+            outer.eval("js", "inner = Java.type('org.graalvm.polyglot.Context').create()");
+            outer.eval("js", "inner.eval('java', System.out.println('Hello inner Java!');");            
+            outer.eval("js", "inner.close()");
+           
         }
     }
 }
