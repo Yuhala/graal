3985c7b29414d997a6c6d74d9e1e1a3b6299574e
Yuhala <petersonyuhala@gmail.com>
2022-01-20 08:55:47 +0100

    moved poly folder here

---
+++ poly/HelloPolyglot.java
@@ -0,0 +1,12 @@
+import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.proxy.*;
+
+public class HelloPolyglot {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        try (Context context = Context.create()) {
+		System.out.println("About to run js context");
+    		context.eval("js", "print('Hello JavaScript!');");
+        }
+    }
+}
