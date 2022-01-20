7eaca93e90d1e2ec44dd6266c476717642c1bb80
Yuhala <petersonyuhala@gmail.com>
2022-01-20 11:04:39 +0100

    testing new env script

--- set-env-graal.sh
+++ set-env-graal.sh
@@ -7,5 +7,13 @@
 #export PATH=$PWD/vm/latest_graalvm/graalvm-a7a83e7967-java11-22.1.0-dev/bin:$PATH
 #export JAVA_HOME=$PWD/vm/latest_graalvm/graalvm-a7a83e7967-java11-22.1.0-dev
 
-export PATH=$PWD/vm/latest_graalvm/graalvm-*-dev/bin:$PATH
-export JAVA_HOME=$PWD/vm/latest_graalvm/graalvm-*-dev
+#
+# Each graal build has a different name so we need a more practical
+# way of adding the new directory to the path
+#
+
+for d in $PWD/vm/latest_graalvm
+    do
+        export PATH="$d/bin:$PATH"
+        export JAVA_HOME="$d"
+    done
