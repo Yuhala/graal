469ede5eb8974deec5892954255ef5e11698f117
Yuhala <petersonyuhala@gmail.com>
2022-01-26 09:21:29 +0100

    testing polyglot program

--- poly/HelloPolyglot.java
+++ poly/HelloPolyglot.java
@@ -5,13 +5,23 @@ import org.graalvm.polyglot.proxy.*;
 public class HelloPolyglot {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
+        // context without try block
+        Context polyglot = Context.create();
+        Value array = polyglot.eval("js", "[1,2,42,4]");
+        int result = array.getArrayElement(2).asInt();
+        System.out.println("Result is: " + result);
+
+        int myInt = polyglot.eval("js", "2").asInt();
+        System.out.println("MyInt is: " + myInt);
+
+    }
+
+    static void contextWithTry() {
         try (Context context = Context.create()) {
             System.out.println("About to run js context");
             context.eval("js", "print('Hello JavaScript!');");
+
         }
 
-       
     }
-
-   
 }
